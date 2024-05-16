5be7dd1c9b0c4ca84d2876f6eb009e4946f5a6c3
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-05-16 05:10:22 -0700

    update

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -20,6 +20,25 @@ def close_db(error):
     storage.close()
 
 
+@app.route('/')
+def hbnb_filters(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
+
+
 @app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     # Retrieve states, amenities, and places from storage
